64aebbb016b047b03d9505c4e97b3e0c08e1292b
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2021-02-28 15:17:54 -0500

    add freesurfer report, failing due to BIDS entity mis-specification

--- niviz/interfaces/views.py
+++ niviz/interfaces/views.py
@@ -3,10 +3,17 @@
 
 from __future__ import annotations
 from typing import TYPE_CHECKING
+import os
+
+if TYPE_CHECKING:
+    from nibael.nifti1 import Nifti1Image
 
 import niworkflows.interfaces.report_base as nrc
-from nipype.interfaces.base import File, traits, InputMultiPath
+from nipype.interfaces.base import File, traits, InputMultiPath, Directory
 from nipype.interfaces.mixins import reporting
+
+import nilearn.image
+import nibabel as nib
 
 from ..node_factory import register_interface
 """
@@ -60,6 +67,8 @@ class IRegRPT(nrc.RegistrationRC):
         Generate transition report as a side-effect. No operations
         are performed on the data (identity)
 
+        If a 4D image is passed in the first index will be pulled for viewing
+
         Args:
             runtime: Nipype runtime object
 
@@ -69,8 +78,13 @@ class IRegRPT(nrc.RegistrationRC):
 
         """
 
-        self._fixed_image = self.inputs.bg_nii
-        self._moving_image = self.inputs.fg_nii
+        # Need to 3Dify 4D images and re-orient to RAS
+        fi = _reorient_to_ras(
+            _make_3d_from_4d(nilearn.image.load_img(self.inputs.fg_nii)))
+        bi = _reorient_to_ras(
+            _make_3d_from_4d(nilearn.image.load_img(self.inputs.bg_nii)))
+        self._fixed_image = fi
+        self._moving_image = bi
 
         return super(IRegRPT, self)._post_run_hook(runtime)
 
@@ -156,10 +170,13 @@ class ISegRPT(nrc.SegmentationRC):
 
         """
 
+        if not isinstance(self.inputs.seg_files, list):
+            self.inputs.seg_files = [self.inputs.seg_files]
+
         # Set variables for `nrc.SegmentationRC`
-        self._anat_file = self.inputs.anat_file
-        self._seg_files = self.inputs.seg_files
-        self._mask_file = self.inputs.mask_file or None
+        self._anat_file = _reorient_to_ras(self.inputs.anat_file)
+        self._seg_files = [_reorient_to_ras for s in self.inputs.seg_files]
+        self._mask_file = _reorient_to_ras(self.inputs.mask_file) or None
         self._masked = self.inputs.masked
 
         # Propogate to superclass
@@ -181,7 +198,107 @@ class ISegRPT(nrc.SegmentationRC):
         return runtime
 
 
+class _IFSCoregInputSpecRPT(nrc._SVGReportCapableInputSpec):
+
+    bg_nii = File(exists=True,
+                  usedefault=False,
+                  resolve=True,
+                  desc='Background NIFTI for SVG',
+                  mandatory=True)
+
+    fg_nii = File(exists=True,
+                  usedefault=False,
+                  resolve=True,
+                  desc='Foreground NIFTI for SVG',
+                  mandatory=True)
+
+    fs_dir = Directory(exists=True,
+                       usedefault=False,
+                       resolve=True,
+                       desc='Subject freesurfer directory',
+                       mandatory=True)
+
+
+class _IFSCoregOutputSpecRPT(reporting.ReportCapableOutputSpec):
+    pass
+
+
+class IFSCoregRPT(nrc.RegistrationRC):
+
+    input_spec = _IFSCoregInputSpecRPT
+    output_spec = _IFSCoregOutputSpecRPT
+
+    def _post_run_hook(self, runtime: Bunch) -> Bunch:
+        """Side-effect function of IFSCoregRPT.
+
+        Generates Freesurfer-based EPI2T1 coregistration report
+        Args:
+            runtime: Nipype runtime object
+
+        Returns:
+            runtime: Resultant runtime object propogated through ReportCapable
+            interfaces
+
+        """
+
+        self._fixed_image = _reorient_to_ras(self.inputs.bg_nii)
+        self._moving_image = _reorient_to_ras(self.inputs.fg_nii)
+        self._contour = os.path.join(self.inputs.fs_dir, 'mri', 'ribbon.mgz')
+
+        return super(IFSCoregRPT, self)._post_run_hook(runtime)
+
+    def _run_interface(self, runtime: Bunch) -> Bunch:
+        """Does nothing.
+
+        Implements identity operation. IFSCoregRPT expects
+        fully registered inputs, so no operations are performed.
+
+        Args:
+            runtime: Nipype runtime object
+
+        Returns:
+            runtime: Resultant runtime object (unchanged)
+
+        """
+        return runtime
+
+
+def _make_3d_from_4d(nii: Nifti1Image, ind: int = 0) -> Nifti1Image:
+    '''
+    Convert 4D Image into 3D one by pulling a single volume.
+    Performs identity mapping if input image is 3D
+
+    Args:
+        nii: Input image
+        ind: Index to pull from 4D image
+    '''
+
+    if len(nii.shape) < 4:
+        return nii
+
+    return nii.slicer[:, :, :, ind]
+
+
+def _reorient_to_ras(img: Nifti1Image) -> Nifti1Image:
+    '''
+    Re-orient image to RAS
+
+    Args:
+        img: Image to re-orient to match ref image
+
+    Returns:
+        img re-oriented to RAS
+    '''
+
+    ras_ornt = nib.orientations.axcodes2ornt(('R', 'A', 'S'))
+    img_ornt = nib.orientations.axcodes2ornt(
+        nib.orientations.aff2axcodes(img.affine))
+    img2ref = nib.orientations.ornt_transform(img_ornt, ras_ornt)
+    return img.as_reoriented(img2ref)
+
+
 # Register interfaces with adapter-factory
 def _run_imports() -> None:
     register_interface(IRegRPT, 'registration')
     register_interface(ISegRPT, 'segmentation')
+    register_interface(IFSCoregRPT, 'freesurfer_coreg')

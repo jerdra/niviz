f637aa7b0c5cb5b4738a1ef7d9ce4c96572470f3
benselby <Ben.Selby@camh.ca>
2021-05-14 12:47:41 -0400

    Adding interface for surface-colume coregistration reports and an example notebook

--- niviz/interfaces/views.py
+++ niviz/interfaces/views.py
@@ -11,14 +11,20 @@ if TYPE_CHECKING:
 import niworkflows.interfaces.report_base as nrc
 from niworkflows.viz.utils import cuts_from_bbox
 from nipype.interfaces.base import File, traits, InputMultiPath, Directory
+from traits.trait_types import BaseInt
 from nipype.interfaces.mixins import reporting
 
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 import nilearn.image
+import nilearn.plotting as nplot
 import nibabel as nib
 import numpy as np
 import trimesh
 
-from ..node_factory import register_interface
+# from ..node_factory import register_interface
+
+
 """
 ReportCapable concrete classes for generating reports as side-effects
 """
@@ -135,7 +141,7 @@ class _ISegInputSpecRPT(nrc._SVGReportCapableInputSpec):
                      resolve=True,
                      desc='ROI Mask for mosaic',
                      mandatory=False)
-
+    
     masked = traits.Bool(False,
                          usedefault=True,
                          desc='Flag to indicate whether'
@@ -309,7 +315,7 @@ def _run_imports() -> None:
     register_interface(IFSCoregRPT, 'freesurfer_coreg')
     register_interface(ISurfVolRPT, 'surface_coreg')
 
-class _ISurfStructInputSpecRPT(nrc._SVGReportCapableInputSpec):
+class _ISurfVolInputSpecRPT(nrc._SVGReportCapableInputSpec):
     '''
     Input spec for reports coregistering surface and volume images
 
@@ -326,14 +332,22 @@ class _ISurfStructInputSpecRPT(nrc._SVGReportCapableInputSpec):
                   desc='Foreground NIFTI for SVG'
                   )
 
-    fs_dir = Directory(exists=True,
-                       usedefault=False,
-                       resolve=True,
-                       desc='Subject freesurfer directory',
-                       mandatory=True)
+    surf_l = File(exists=True,
+                  usedefault=False,
+                  resolve=True,
+                  desc='Left surface file',
+                  mandatory=True)
+
+    surf_r = File(exists=True,
+                  usedefault=False,
+                  resolve=True,
+                  desc='Right surface file',
+                  mandatory=True)
+
+    n_cuts = BaseInt(desc='Number of slices to display')
 
 
-class _ISurfStructOutSpecRPT(reporting.ReportCapableOutputSpec)
+class _ISurfVolOutputSpecRPT(reporting.ReportCapableOutputSpec):
     pass
 
 
@@ -379,71 +393,77 @@ class SurfVolRC(reporting.ReportCapableInterface):
 
 
 class ISurfVolRPT(SurfVolRC):
-	'''
-	Report interface for co-registered surface/volumetric images 
-	'''
-	input_spec = _ISurfVolInputSpecRPT
+    '''
+    Report interface for co-registered surface/volumetric images 
+    '''
+    input_spec = _ISurfVolInputSpecRPT
     output_spec = _ISurfVolOutputSpecRPT
-    volname = {
-        "T1w": "T1w_acpc_dc.nii.gz",
-        "MNINonLinear": "T1w.nii.gz"
-    }
-
-	shortname = {
-        "Native": 'native',
-        "fsaverage_LR32k": "32k_fs_LR",
-        "": "164k_fs_LR"
-    }	
 
     def _post_run_hook(self, runtime):
         outputs = self.aggregate_outputs(runtime=runtime)
         
         self._bg_nii = self.inputs.bg_nii
         self._fg_nii = self.inputs.fg_nii or None
-        self._fs_dir = self.inputs.fs_dir
-        self._reg_space = self.inputs.reg_space or 'T1w'
-        self._surf_space = self.inputs.surf_space or 'fsaverage_LR32k'
-        self._vol_name = volname[self._reg_space]
+        self._surf_l = self.inputs.surf_l       
+        self._surf_r = self.inputs.surf_r       
+        self._ncuts = self.inputs.n_cuts or 7
 
         # Propogate to superclass
-        return super(ISurfSegRPT, self)._post_run_hook(runtime)
+        return super(ISurfVolRPT, self)._post_run_hook(runtime)
 
     def _run_interface(self, runtime):
         return runtime
-  
+ 
+    def get_shortname(self):
+        return self.shortname
+
     def _generate_report(self):
         '''Make a composite for co-registration of surface and volume images'''
         
-  	    l_surf = nib.load(
-  	    			os.path.join(
-	          		      self._fs_dir, self._reg_space, self._surf_space,
-	    		  		  f'{sub}.L.pial.{shortname[self._surf_space]}.surf.gii'))			  		
-  	    r_surf = nib.load(
-                    os.path.join(
-    		  		      self._fs_dir, self._reg_space, self._surf_space,
-	    		  		  f'{sub}.R.pial.{shortname[self._surf_space]}.surf.gii'))			  		
-        vol_img = nib.load( 
-                      os.path.join( self._fs_dir, self._reg_space, self._vol_name))
+        l_surf = nib.load(self._surf_l)
+        r_surf = nib.load(self._surf_r)
+        vol_img = nib.load(self._bg_nii)
+
+        if vol_img.ndim==4:
+            vol_img = vol_img.slicer[:,:,:,0]
         
         l_verts, l_trigs = gifti_get_mesh(l_surf)
         r_verts, r_trigs = gifti_get_mesh(r_surf)
         verts, trigs, offset = gifti_get_full_brain(l_surf, r_surf)
         
         mesh = trimesh.Trimesh(vertices=verts, faces=trigs)
-        mask_nii = nimg.threshold_img(vol_img, 1e-3)
+        mask_nii = nilearn.image.threshold_img(vol_img, 1e-3)
         cuts = cuts_from_bbox(mask_nii, cuts=self._ncuts)
 
         sections = mesh.section_multiplane(
                         plane_normal=[0,0,1],
                         plane_origin=[0,0,0],
                         heights=cuts['z'])
-          
-  	    zh = nplot.plot_anat(vol_img, display_mode='z', cut_coords=cuts['z'])
-  	    
-  	    for z, s in zip(cuts['z'], sections):
-  	        ax = zh.axes[z].ax
-  	        if s:
-  	            for segs in s.discrete:
-  	                ax.plot(*segs.T, color='r', linewidth=0.5) 
+
+        zh = nplot.plot_anat(vol_img, display_mode='z', cut_coords=cuts['z'])
+
+        for z, s in zip(cuts['z'], sections):
+            ax = zh.axes[z].ax
+            if s:
+                for segs in s.discrete:
+                    ax.plot(*segs.T, color='r', linewidth=0.5) 
+        
+        if self._fg_nii:
+            fg_img = nib.load(self._fg_nii).slicer[:,:,:,0]
+            fg_img = nilearn.image.resample_to_img(fg_img, vol_img,
+                    interpolation="linear") 
+            # Custom colormap with transparencies
+            ncolors = 256
+            basecmap = 'viridis_r'
+            color_array = plt.get_cmap(basecmap)(range(ncolors))
+            color_array[:, -1] = np.linspace(1.0, 0.0, ncolors)
+            
+            # Set background intensity=0 to transparent
+            color_array[0, :] = 0
+            cmapviridis = mcolors.LinearSegmentedColormap.from_list(
+                basecmap,colors=color_array
+            )
+
+            zh.add_overlay(fg_img, cmap=cmapviridis)
 
         zh.savefig(self._out_report)
